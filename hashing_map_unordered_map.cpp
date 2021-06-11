9ef65fafae57ac90db804014f1f394f1afb9547e
Abhik Das <abhikdas98765@gmail.com>
2021-06-11 15:33:28 +0530

    added hashing problems

---
+++ hashing_map_unordered_map.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    map<int, int> m;
+
+    m[8] = 2;
+    cout<<m[8]<<endl;
+
+    unordered_map<int, int> um;
+
+    um[3] = 6;
+    cout<<um[3]<<endl;
+
+    return 0;
+}
+
---
+++ number_of_subarrays_with_sum0.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+
+    map<int, int> count;
+    int prefSum = 0;
+
+    for(int i=0; i<n; i++) {
+        prefSum += a[i];
+        count[prefSum]++;
+    }
+
+    int ans = 0;
+    map<int, int> :: iterator it;
+    
+    for(it = count.begin(); it!=count.end(); it++) {
+        int c = it->second;
+        ans += (c*(c-1))/2;
+
+        if(it->first == 0)
+            ans += it->second;
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
